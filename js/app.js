e341b00b5ec49b725290ead0d70be8232a6d8eb4
PaulineCty <paulinecauty@gmail.com>
2022-12-03 11:30:35 +0100

    nav links JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//RESUME LINKS
+
 const headerResumeDiv = document.querySelector('.header__resume');
 const headerResumeLinks = document.querySelector('.header__resume-links');
 
@@ -26,7 +28,35 @@ headerResumeDiv.addEventListener('mouseout', handleMouseOutHeader);
 contactResumeDiv.addEventListener('mouseover', handleMouseOverContact);
 contactResumeDiv.addEventListener('mouseout', handleMouseOutContact);
 
-//when finish hovering display:none;
+
+//NAV LINKS 
+
+const navItems = document.querySelectorAll('.header__nav-item');
+
+function handleMouseOverNav(event) {
+    let navList = ['homeNav','experienceNav','aboutNav','contactNav']; //not nice, would be great to get them differently
+    const target = event.target.id;
+    const targetIndex = navList.indexOf(target);
+    navList.splice(targetIndex,1);
+    for(let i=0 ; i<navList.length ; i++){
+        navList[i] = '#' + navList[i];
+        document.querySelector(navList[i]).style.color = 'rgba(229, 221, 203,0.5)';
+    }
+}
+
+function handleMouseOutNav(event) {
+    let navList = ['homeNav','experienceNav','aboutNav','contactNav']; //not nice, would be great to get them differently
+    for(let i=0 ; i<navList.length ; i++){
+        navList[i] = '#' + navList[i];
+        document.querySelector(navList[i]).style.color = 'rgba(229, 221, 203)';
+    }
+}
+
+for(let i=0 ; i<navItems.length ; i++) {
+    navItems[i].addEventListener('mouseover', handleMouseOverNav);
+    navItems[i].addEventListener('mouseout', handleMouseOutNav);
+}
+
+
 //faire truc menu qui passe en gris l'actuel sections
-//header : animation hover links comme tamalsen.dev
 //truc scroll pour s'amuser
